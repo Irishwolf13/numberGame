306012b8219ccc9c65d5967b687b67f4385fb409
John Rooney <rooneyjohn@gmail.com>
2023-08-02 15:42:09 -0400

    put in a fallback, goes to blank page currently

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   # Added per instructinos
   get '/hello', to: 'application#hello_world'
+  
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
